32feffbf222fb119f700ccc0c06ec2d1048e4c6e
Otaku4fun <141166008+Otaku4fun@users.noreply.github.com>
2023-08-02 17:57:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 API_ID = int(os.environ.get("API_ID", "24556417"))
 API_HASH = os.environ.get("API_HASH", "24008c23b7506a0bd8821f19c19cd654")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6583287452:AAFmZlFTX0tFE7VhQ34zHigf_eIS3Tvw4D0")
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1001627581573")
+DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "1627581573")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID", 6013634182))
 PROTECT_CONTENT = True
